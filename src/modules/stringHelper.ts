51150253c231cce79228f72c98e0e6c0e1190e8a
LateDream <127290936+LateDreamXD@users.noreply.github.com>
2025-04-08 16:18:20 +0800

    🐞 fix(stringHelper): full width symbols scope

--- src/modules/stringHelper.ts
+++ src/modules/stringHelper.ts
@@ -3,10 +3,15 @@ import aysConfig from '../../ays.config';
 String.prototype.countSymbols = function() {
 	let fullWidthCount = 0, halfWidthCount = 0;
 	if(this.length === 0) return {fullWidthCount, halfWidthCount};
+	const isCJK = (code: number) =>
+		  code >= 0x3000 && code <= 0x303F || // 中文标点
+		  code >= 0x4E00 && code <= 0x9FFF;   // 汉字
 
-	for(let char of this) {
-		if(char.charCodeAt(0) >= 0xFF00 && char.charCodeAt(0) <= 0xFFEF)
+	for(const char of this) {
+		if(isCJK(char.charCodeAt(0)) || 
+			(char.charCodeAt(0) >= 0xFF00 && char.charCodeAt(0) <= 0xFFEF)) {
 			fullWidthCount++;
+		}
 		if(char.charCodeAt(0) >= 0x0020 && char.charCodeAt(0) <= 0x007E)
 			halfWidthCount++;
 	}
